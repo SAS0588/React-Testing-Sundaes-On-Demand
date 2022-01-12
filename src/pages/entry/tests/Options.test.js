e10348757fa020206d3bfd883bfe754158e835f1
Saad Sharieff <saadsharieff@google.com>
2022-01-12 13:39:00 -0600

    Code and test scoop options

--- src/pages/entry/tests/Options.test.js
+++ src/pages/entry/tests/Options.test.js
@@ -2,11 +2,11 @@ import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
 describe("Options Test", () => {
-  test("displays image for each scoop p=option from server", () => {
+  test("displays image for each scoop p=option from server", async () => {
     render(<Options optionType="scoops" />);
 
     // find images
-    const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+    const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
     expect(scoopImages).toHaveLength(2);
 
     // confirm alt text of images
