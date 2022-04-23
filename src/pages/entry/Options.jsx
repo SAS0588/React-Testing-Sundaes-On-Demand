94434122689bd9c61c2197b62531f5b4962160fe
Saad Sharieff <saadsharieff@google.com>
2022-04-22 22:57:27 -0500

    started writing tests for sundae app

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,18 +3,23 @@ import { useEffect, useState } from "react";
 import ScoopOptions from "./ScoopOptions";
 import ToppingOptions from "./ToppingOptions";
 import { Row } from "react-bootstrap";
+import AlertBanner from "../common/AlertBanner";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: handle error later
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   // Replace null with ToppingOption
   const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions;
